47f58a32c16344d33abbbfb8565685038b48a8d3
Erik Wyatt <wyatte@stolaf.edu>
2015-05-05 19:03:29 +0200

    milestone 2: packet loss and node failures

--- README.md
+++ README.md
@@ -2,12 +2,23 @@
 We simulate a network of database servers, who would like to store some
 information but are plagued by random failures.
 
+The evolution of the plan (in this file) is at least as interesting as
+the code itself. Some noteworthy things about the code:
+- heavily async JS is readable because of futures
+- the code has at least one of everything: a class, a unit test, etc
+
 ## Build and run
-You need to have the typescript compiler installed already.
-`tsc *.ts --module commonjs && node main.js`
+You need to have the typescript compiler and npm installed
+already. Clone this repo and `cd` to it.
+Once, to set things up: `npm install q`
+Every time you make a code change: `tsc *.ts --module commonjs && node main.js`
 
 ##Development plan
-- Milestone 1: client and server, client sends data to server, server stores data, client retrieves data 
-- Milestone 2: introduce latency, dropped packets, and node failures
-- Milestone 3/Finished: The DB uses multiple servers and is tolerates
+- Milestone 1: client and server, client sends data to server, server stores data, client retrieves data -- DONE
+- Milestone 2 -- MVP version of failure: dropped packets, and simple version of node failures, the integration test is statistical and handles flakiness well
+- Milestone 3: The DB uses multiple servers and is tolerates
   the faults
+- Milestone 4: Implement time. Introduce latency, and more realistic
+  node failure.
+
+main.js will be split to multiple files if it grows beyond 500 lines
--- main.ts
+++ main.ts
@@ -1,9 +1,46 @@
+/// <reference path="Q.d.ts" />
 import assert = require("assert");
+import Q = require('q');
 
 class Server {
 	data:string;
+	dead:boolean = false;
+	static sqrtPacketLoss = .9;
+	//This is unrealistic -- nodes should fail based on time, not based on
+	//how much we talk to them
+	static nodeFailure = .99
 	constructor () {}
-	send(command: string, data?: string) {
+
+	//Wrapper around handle that introduces failures
+	send(command: string, data?: string):Q.Promise<string> {
+		return this.sendTestable(command, data, Math.random(), Math.random(), Math.random());
+	}
+
+	sendTestable(command: string, data: string, nodeFailureRandomness: number, requestDropRandomness: number, responseDropRandomness: number):Q.Promise<string> {
+		var deferred = Q.defer();
+		deferred.reject(new Error("somebody is trying to wait forever"));
+		var emptyPromise = <Q.Promise<string>>deferred.promise;
+		if (this.dead) {
+			return emptyPromise;
+		}
+		if (nodeFailureRandomness > Server.nodeFailure) {
+			this.dead = true;
+			return emptyPromise;
+		}
+		if (requestDropRandomness < Server.sqrtPacketLoss) {
+			var response = Q.fcall(this.handle, command, data);
+			if (responseDropRandomness < Server.sqrtPacketLoss) {
+				return response;
+			} else {
+				return emptyPromise;
+			}
+		} else {
+			return emptyPromise;
+		}
+	}
+
+
+	handle(command: string, data?: string) {
 		if (command === "set") {
 			this.data = data;
 			return "OK";
@@ -15,14 +52,55 @@ class Server {
 	}
 }
 
-var testSuccess = false;
-function main() {
+function runIntegrationTestOnce():Q.Promise<string> {
 	var server = new Server();
-	var ack = server.send("set", "test");
-	assert.equal(ack, "OK");
-	var response = server.send("get");
-	assert.equal(response, "test");
-	console.log("The test passed.")
+	return server.send("set", "test")
+	.then(function(response) {
+		assert.equal(response, "OK");
+		return server.send("get");
+	}).then(function(response) {
+		assert.equal(response, "test");
+		return "";
+	});
 }
 
-main();
+function runIntegrationTest() {
+	var testFailures = 0;
+	var n = 100;
+	var promises:Q.Promise<string>[] = [];
+	for (var i = 0; i < n; i++) {
+		promises.push(runIntegrationTestOnce()
+		.fail(function(error: Error) {
+			testFailures++;
+			return "";
+		}));
+	}
+	Q.all(promises).then(function () {
+		var successRate = (n - testFailures) / n
+		console.log("Integration test success rate: ")
+		console.log(successRate)
+	}).done();
+}
+
+function assertPromiseIsError(promise:Q.Promise<string>):Q.Promise<string> {
+	return promise.then(function ():string {
+		throw Error("the promise was supposed to contain an error");
+	}, function () {
+		return "";
+	});
+}
+
+function testSend():Q.Promise<any> {
+	var everythingOk = new Server().sendTestable("set", "foo", 0, 0, 0);
+	everythingOk.done();
+	var nodeFailure = assertPromiseIsError(new Server().sendTestable("set", "foo", 1, 0, 0));
+	var requestDropped = assertPromiseIsError(new Server().sendTestable("set", "foo", 0, 1, 0));
+	var responseDropped = assertPromiseIsError(new Server().sendTestable("set", "foo", 0, 0, 1));
+	return Q.all([everythingOk, nodeFailure, requestDropped, responseDropped]);
+}
+
+function runUnitTests():Q.Promise<any> {
+	return testSend();
+}
+
+runUnitTests().then(runIntegrationTest).done();
